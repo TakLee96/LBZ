32c1773bea673937da8ea47f63bcd282b35d0972
Jiahang Li <jiahang_li@outlook.com>
2016-04-30 14:24:42 -0700

    [Tak] change children implementation

--- lbz/ApproxSolver.java
+++ lbz/ApproxSolver.java
@@ -1,32 +1,41 @@
 package lbz;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 
 /** The approximate solver for MWIS problem.
  * @author Jim Bai, Tak Li, Zirui Zhou */
 public class ApproxSolver {
 
     public static Iterable<Cycle> solve(CycleGraph cg) {
-
         HashSet<Integer> effective = new HashSet<Integer>();
         for (int i = 0; i < cg.getNumVertices(); i++) {
             effective.add(i);
         }
 
-        int maxrank, maxindex, rank;
-        while (not effective.isEmpty()) {
+        LinkedList<Cycle> solution = new LinkedList<Cycle>();
+
+        int maxindex; double maxrank, rank;
+        while (!effective.isEmpty()) {
             maxrank = 0;
-            rank = -1;
             for (int i : effective) {
-                rank = cg.getCycle(i).getWeight();
-                if (weight > maxweight) {
-                    maxweight = weight;
+                rank = 1.0 * cg.getCycle(i).getWeight() /
+                    (cg.getNumNeighbors(i) + 1);
+                if (rank > maxrank) {
+                    maxrank = rank;
                     maxindex = i;
                 }
             }
-
+            solution.add(cg.getCycle(maxindex));
+            for (int n : cg.neighbors(maxindex)) {
+                cg.remove(n);
+                effective.remove(n);
+            }
+            cg.remove(maxindex);
+            effective.remove(maxindex);
         }
 
+        return solution;
     }
 
 }
--- lbz/Constants.java
+++ lbz/Constants.java
@@ -4,6 +4,8 @@ package lbz;
  * @author Jim Bai, Tak Li, Zirui Zhou */
 public class Constants {
 
+    public static final int numVerticesThreshold = 50;
+
     public static final double v2eThreshold = 1.5;
 
     public static final double cycleThreshold = 21;
