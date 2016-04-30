cb34cf4b156191e24e4ec87d17378a4f2ca5ea27
Zirui <zirui_zhou@berkeley.edu>
2016-04-30 21:25:57 +0000

    [ZZY] implementing find cycle

--- lbz/CycleGraph.java
+++ lbz/CycleGraph.java
@@ -8,15 +8,38 @@ public class CycleGraph extends Graph {
 
     private static ArrayList<Cycle> extractCycles(DonationGraph g) {
         /* TODO: Extract all the cycles in g */
-        int v=g.getNumVertices();
-        for (int i=1; i<v+1; i++){
-            new Cycle();
+        int v = g.getNumVertices();
+        ArrayList<Cycle> result = new ArrayList<Cycle>();
+        for (int i = 1; i < v+1; i++) {
+            int root = i;
+            ArrayList<Cycle> cur = CycleGraph.helper(5, new ArrayList<int>, root, g, result);
+            for ()
         }
         return null;
     }
     
+    private static ArrayList<Cycle> helper(int weight, int deg, ArrayList<int> path, int root, DonationGraph g, HashSet<Cycle> partialRes) {
+        for(int i = deg; i>0; i--) {
+            for (int vertex: g.neighbors(i)) {
+                int w = 0;
+                if (g.isChild(v)) { 
+                    w=2;
+                }
+                else w=1;
+                if(vertex==target) {
+                    ArrayList<int> p = new ArrayList<int>();
+                    p.add(vertex);
+                    partialRes.add(new Cycle(p, weight+w));
+                } 
+                helper(weight+w, )
+            }
+        }
+        return null;
+    }
+    
+    
     private ArrayList<Cycle> cycles;
-    public getCycle(int i) {
+    public Cycle getCycle(int i) {
         return cycles.get(i);
     }
 
