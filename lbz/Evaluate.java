104eca151d6fbbad7e04f1c11c6381396efd9ec2
Jiahang Li <jiahang_li@outlook.com>
2016-05-01 11:50:51 -0700

    [Tak] another run

--- lbz/Evaluate.java
+++ lbz/Evaluate.java
@@ -1,6 +1,6 @@
 package lbz;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 /** Evaluate the solution
  * @author Jim Bai, Tak Li, Zirui Zhou */
@@ -9,17 +9,23 @@ public class Evaluate {
     public static int score(DonationGraph g, Iterable<Cycle> cycles) {
         int maxscore = g.getNumVertices() + g.getNumChildren();
         int total = 0;
-        HashSet<Integer> sanity = new HashSet<Integer>();
+        boolean repeat = false;
+        HashMap<Integer, Cycle> sanity = new HashMap<Integer, Cycle>();
         for (Cycle c : cycles) {
             for (int v : c.getVertices()) {
-                if (!sanity.contains(v)) {
-                    sanity.add(v);
+                if (!sanity.containsKey(v)) {
+                    sanity.put(v, c);
                 } else {
+                    System.out.println(sanity.get(v));
                     System.out.println(c);
-                    throw new RuntimeException("repetition error");
+                    repeat = true;
+                    System.out.println("----REPETITION----");
                 }
             }
             total += c.weight(g);
+        }
+        if (repeat) {
+            return 213;
         }
         return total - maxscore;
     }
