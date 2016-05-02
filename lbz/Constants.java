c2e0b49ed57a26b81de7cd0ca7df3e67acc18f2f
Jim Bai <jimbai@berkeley.edu>
2016-05-01 22:19:47 -0700

    fix exactsolver bug

--- lbz/Constants.java
+++ lbz/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
 
     public static final int maxSearchCycle = 10000;
 
-    public static final int maxExactCycle = 50;
+    public static final int maxExactCycle = 100;
 
 }
